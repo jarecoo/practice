1d134183fc3f222d85b173b0433cb5b9596f7f6f
Jared <jarecoo@gmail.com>
2014-02-18 16:11:24 -0700

    intital dog specs and class

--- tdd_and_rspec/spec/calculator_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "calculator" do
-
-  it "can add" do
-    c = Calculator.new
-  end
-
-end
---
+++ tdd_and_rspec/spec/lib/dog_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+require "lib/dog"
+
+describe Dog do
+
+  it "has a name" do
+    dog = Dog.new
+    dog
+  end
+
+end
