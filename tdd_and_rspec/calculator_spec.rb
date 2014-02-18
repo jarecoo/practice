f0e2025804cde5fe1acffa040e3fc81ddb6c45f8
Jared <jarecoo@gmail.com>
2014-02-18 15:35:59 -0700

    begin learning rspec

---
+++ tdd_and_rspec/calculator_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'calculator.rb'
+
+describe Calculator do
+
+  it "can add" do
+
+  end
+
+end
---
+++ tdd_and_rspec/spec/calculator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "calculator" do
+
+  it "can add" do
+    c = Calculator.new
+  end
+
+end
