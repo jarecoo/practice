9001b103b26a13d841c820b708c72d6321068740
Jared Cook <jarecoo@gmail.com>
2014-02-19 14:11:04 -0700

    new version of doge

--- tdd_and_rspec/spec/lib/doge.rb
+++ tdd_and_rspec/spec/lib/doge.rb
@@ -1,8 +1,20 @@
 class Doge
-attr_accessor :name
+  attr_accessor :name, :age
 
-  def speak
-    "much test, very TDD, wow, such red/green/repeat"
+  def initialize(name="rufis")
+    @name = name
+  end
+
+  def bark
+    "woof"
+  end
+
+  def urinate
+    true
+  end
+
+  def dog_age
+    age * 7
   end
 
 end
--- tdd_and_rspec/spec/lib/doge_spec.rb
+++ tdd_and_rspec/spec/lib/doge_spec.rb
@@ -1,16 +1,31 @@
-require "spec_helper"
-require "lib/doge"
+require 'spec_helper'
+require 'lib/doge'
 
 describe Doge do
-let(:doge){ Doge.new }
-
-  it "has a name" do
-    doge.name = "Doge"
-    doge.name.should == "Doge"
-  end
+let(:doge){ Doge.new("Such wow") }
 
   it "can bark" do
-    doge.speak.should == "much test, very TDD, wow, such red/green/repeat"
+    doge.bark.should == "woof"
+  end
+
+  it "urinate" do
+    doge.urinate.should be_true
+  end
+
+  it "has a name" do
+    doge.name.should_not be_nil
+    doge.name.should == "Such wow"
+  end
+
+  it "has an age" do
+    doge.age = 5
+    doge.age.should_not be_nil
+    doge.dog_age.should_not be_nil
+    doge.dog_age.should == 35
+  end
+
+  it "can find a leg" do
+    doge.leg.should_not be_nil
   end
 
 end
