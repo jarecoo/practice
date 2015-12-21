c1983c3315183d25928c1cdf79514f3e8b80b4d7
Jared Cook <jared@masteryconnect.com>
2015-12-21 11:24:33 -0700

    add stats to collatz

--- collatz_conjecture.rb
+++ collatz_conjecture.rb
@@ -6,6 +6,8 @@ if n == 0
   puts "must give an actual number greater than 0"
 else
   i = 0
+  largest = n
+  starting_number = n
   while n != 1 do
     if n.even?
       n = n/2
@@ -13,9 +15,12 @@ else
       n = 3 * n + 1
     end
     puts n
+    largest = n if n > largest
     i += 1
   end
 
+  puts "starting number: #{starting_number}"
   puts "number of steps: #{i}"
+  puts "largest number in series: #{largest}"
 end
 
