34d95b3cef9532f9d74ad8024798c8b65a0baa6e
Jared Cook <jaredcook@Stephs-MacBook-Pro.local>
2024-04-16 21:44:41 -0600

    add instructions

--- game_play_network/run_poker_app.rb
+++ game_play_network/run_poker_app.rb
@@ -1,5 +1,7 @@
 ## run poker app
+# launch irb in app root folder, including poker app file
 irb -r ./poker_app.rb
 
+# instantiate app and call play_games method
 app = PokerApp.new(File.open("poker.txt"))
 app.play_games_from_file
